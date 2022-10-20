b2f37cf5b881753e78dd16aab9ff7b7e5800b882
Raul Herrera <raulherrera01022003@gmail.com>
2022-10-19 22:56:59 -0500

    Iniciando react

--- iniciando-react/src/App.jsx
+++ iniciando-react/src/App.jsx
@@ -1,59 +1,59 @@
 import React, { Fragment, useState, useRef, useEffect } from "react";
-import { v4 as uuid } from 'uuid'
-import { TodoList } from "./components/TodoList"
+import { v4 as uuidv4 } from "uuid";
+import { TodoList } from "./components/TodoList";
 
 const KEY = "todoApp.todos";
 
 export function App() {
-    const [todos, setTodos] = useState([
-        {id: 1, task: "Tarea 1", completed: false}
-    ]);
+  const todoTaskRef = useRef();
+  const [todos, setTodos] = useState([
+    { id: 1, task: "Tarea ", completed: false },
+  ]);
 
-    const todoTaskRef = useRef();
-
-    useEffect(() => {
-        const storedTodos = JSON.parse(localStorage.getItem(KEY));
-        if (storedTodos) {
-            setTodos(storedTodos);
-        }
-    }, [todos]);
-
-    useEffect(() => {
-        localStorage.setItem(KEY, JSON.stringify(todos))
-    }, [todos]);
-
-    const toggleTodo = (id) => {
-        const newTodos = [...todos];
-        const todo = newTodos.find((todo) => todo.id === id);
-        todo.completed = !todo.completed;
-        setTodos(newTodos);
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(KEY));
+    if (storedTodos) {
+      setTodos(storedTodos);
     }
+  }, []);
 
-    const handleTodoAdd = () => {
-        const task = todoTaskRef.current.value;
-        if (task === "") return;
+  useEffect(() => {
+    localStorage.setItem(KEY, JSON.stringify(todos));
+  }, [todos]);
 
-        setTodos((prevTodos) => {
-            return [...prevTodos, {id: uuid(), task, completed: false}];
-        });
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
 
-        todoTaskRef.current.value = null;
-    }
+  const handleTodoAdd = (event) => {
+    const task = todoTaskRef.current.value;
+    if (task === "") return;
 
-    const handleClearCompletedTodos = () => {
-        const todosNotCompleted = todos.filter(todo => !todo.completed);
-        setTodos(todosNotCompleted);
-    }
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), task, completed: false }];
+    });
 
-    return (
-        <Fragment>
-            <TodoList todos={todos} toggleTodo={toggleTodo}/>
-            <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea"/>
-            <button onClick={handleTodoAdd}>+</button>
-            <button onClick={handleClearCompletedTodos}>-</button>
-            <div>
-                Te quedan {todos.filter(todo => !todo.completed).length} tareas por completar
-            </div>
-        </Fragment>
-    );
+    todoTaskRef.current.value = null;
+  };
+
+  const handleClearAll = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
+  return (
+    <Fragment>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoTaskRef} type="text" placeholder="Nueva tarea" />
+      <button onClick={handleTodoAdd}>Añadir</button>
+      <button onClick={handleClearAll}>Eliminar</button>
+      <div>
+        Te quedan {todos.filter((todo) => !todo.completed).length} tareas por
+        terminar
+      </div>
+    </Fragment>
+  );
 }
