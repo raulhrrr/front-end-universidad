d4ffce54676a54dbcdffdcdcd05693340f65faca
Raul Herrera <raulherrera01022003@gmail.com>
2022-10-26 23:54:50 -0500

    Avance responsive

--- consumo-api-rick-and-morty-react/src/pages/home/components/character-card/character-card.jsx
+++ consumo-api-rick-and-morty-react/src/pages/home/components/character-card/character-card.jsx
@@ -8,7 +8,7 @@ export const Character = ({ character }) => {
                 <img src={character.image} alt={character.name} />
             </div>
             <div className="card-content">
-                <div>
+                <div className="section-one">
                     <a href={character.url} rel="nofollow noopener noreferrer" target="_blank">
                         <h2>{character.name}</h2>
                     </a>
@@ -16,20 +16,22 @@ export const Character = ({ character }) => {
                             status`}></span>{character.status.toLowerCase().charAt(0).toUpperCase() +
                         character.status.toLowerCase().slice(1)} - {character.species}</span>
                 </div>
-                <div className="section">
-                    <div>
-                        <span className="text-bold">Género:</span>
-                        <p>{character.gender}</p>
-                    </div>
-                    <div>
-                        <span className="text-bold">Origen:</span>
-                        <a href={character.origin.url} rel="nofollow noopener noreferrer"
-                            target="_blank">{character.origin.name}</a>
-                    </div>
-                    <div>
-                        <span className="text-bold">Localización:</span>
-                        <a href={character.location.url} rel="nofollow noopener noreferrer"
-                            target="_blank">{character.location.name}</a>
+                <div className="section-two">
+                    <div className="subsection">
+                        <div>
+                            <span className="text-bold">Género:</span>
+                            <p>{character.gender}</p>
+                        </div>
+                        <div>
+                            <span className="text-bold">Origen:</span>
+                            <a href={character.origin.url} rel="nofollow noopener noreferrer"
+                                target="_blank">{character.origin.name}</a>
+                        </div>
+                        <div>
+                            <span className="text-bold">Localización:</span>
+                            <a href={character.location.url} rel="nofollow noopener noreferrer"
+                                target="_blank">{character.location.name}</a>
+                        </div>
                     </div>
                 </div>
             </div>
